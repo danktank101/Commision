9eb06e5318525b9ebd2f60336c678eac8e112d9c
Peter Dankov <Pdankov3496@stu.d214.org>
2021-10-01 14:42:11 -0500

    add outlets and actions

--- Commision/ViewController.swift
+++ Commision/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BasePayLabel: UILabel!
+    @IBOutlet weak var CommisionPayTextField: UILabel!
+    @IBOutlet weak var TotalPayLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func WhenButtonPressed(_ sender: Any) {
+    }
+    
 }
 
